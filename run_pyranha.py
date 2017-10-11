504bf62afdddf012b462fb93b455180b3ee61ec2
Ben Thorne <ben.thorne@physics.ox.ac.uk>
2017-10-11 14:49:48 -0400

    Added plotting module.

--- run_pyranha.py
+++ run_pyranha.py
@@ -9,8 +9,20 @@ def main():
     pyr.compute_instrument()
     pyr.compute_cosmology()
     fisher = pyr.fisher()
-    pyranha.plot_fisher(fisher, xcen=0, ycen=1)
+
+    pyr.delensing = True
+    pyr.delensing_factor = 0.1
+    fisher_lens = pyr.fisher()
+
+    pyr.map_res = 0.02
+    pyr.compute_instrument()
+    fisher_fgnd = pyr.fisher()
+
+    fig = pyranha.plot_fisher_1d([fisher, fisher_fgnd, fisher_lens], [r'LiteBIRD', r'LiteBIRD + 2%, foreground', r'LiteBIRD + 90% delensing'])
+    fig.savefig("plots/demonstration.pdf", bbox_inches='tight')
+    fig.savefig("plots/demonstration.png", bbox_inches='tight')
     plt.show()
+
     return
 
 if __name__ == "__main__":
